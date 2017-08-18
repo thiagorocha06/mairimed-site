258e970f8ce3431208cef089ed5d2983ed297c35
Thiago Rocha <thiagorocha06@gmail.com>
2017-08-18 16:37:59 -0300

    Adicionado template e view para detalhe dos artigos e tambem CSS para o site.

--- artigos/urls.py
+++ artigos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.lista_artigos, name='lista_artigos'),
+    url(r'^artigo/(?P<pk>\d+)/$', views.detalhe_artigo, name='detalhe_artigo'),
 ]
--- artigos/views.py
+++ artigos/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Artigo
+from django.shortcuts import render, get_object_or_404
 
 def lista_artigos(request):
     artigos	=	Artigo.objects.filter(data_de_publicacao__lte=timezone.now()).order_by('data_de_publicacao')
     return render(request, 'artigos/lista_artigos.html', {'artigos' : artigos})
+
+def detalhe_artigo(request, pk):
+    artigo = get_object_or_404(Artigo, pk=pk)
+    return render(request, 'artigos/detalhe_artigo.html', {'artigo': artigo})
