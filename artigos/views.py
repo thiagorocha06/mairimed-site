859182d36c49e861e499345c1d6d71ca38f17f87
Thiago Rocha <thiagorocha06@gmail.com>
2017-08-18 15:42:49 -0300

    Adicionado templates para mostrar conteudo dos artigos.

--- artigos/views.py
+++ artigos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Artigo
 
-# Create your views here.
+def lista_artigos(request):
+    artigos	=	Artigo.objects.filter(data_de_publicacao__lte=timezone.now()).order_by('data_de_publicacao')
+    return render(request, 'artigos/lista_artigos.html', {'artigos' : artigos})
