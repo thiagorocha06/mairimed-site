7eb5fb4a00dbbc71da6dfbcfc1c4f7f4292a42f5
Thiago Rocha <thiagorocha06@gmail.com>
2017-08-22 20:52:02 -0300

    Adicionado view e template para artigo detalhado e CSS para o site.

--- artigos/urls.py
+++ artigos/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.lista_artigos, name='lista_artigos'),
     url(r'^artigo/(?P<pk>\d+)/$', views.detalhe_artigo, name='detalhe_artigo'),
+    url(r'^artigo/novo/$', views.novo_artigo, name='novo_artigo'),
+    url(r'^artigo/(?P<pk>\d+)/edicao/$', views.edicao_artigo, name='edicao_artigo'),
+
 ]
--- artigos/views.py
+++ artigos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Artigo
 from django.shortcuts import render, get_object_or_404
+from .forms import ArtigoForm
+from django.shortcuts import redirect
 
 def lista_artigos(request):
     artigos	=	Artigo.objects.filter(data_de_publicacao__lte=timezone.now()).order_by('data_de_publicacao')
@@ -10,3 +12,30 @@ def lista_artigos(request):
 def detalhe_artigo(request, pk):
     artigo = get_object_or_404(Artigo, pk=pk)
     return render(request, 'artigos/detalhe_artigo.html', {'artigo': artigo})
+
+def novo_artigo(request):
+    if request.method == "POST":
+        form = ArtigoForm(request.POST)
+        if form.is_valid():
+            artigo = form.save(commit=False)
+            artigo.author = request.user
+            artigo.data_de_publicacao = timezone.now()
+            artigo.save()
+            return redirect('detalhe_artigo', pk=artigo.pk)
+    else:
+        form = ArtigoForm()
+    return render(request, 'artigos/edicao_artigo.html', {'form': form})
+
+def edicao_artigo(request, pk):
+    artigo = get_object_or_404(Artigo, pk=pk)
+    if request.method == "POST":
+        form = ArtigoForm(request.POST, instance=artigo)
+        if form.is_valid():
+            artigo = form.save(commit=False)
+            artigo.author = request.user
+            artigo.published_date = timezone.now()
+            artigo.save()
+            return redirect('detalhe_artigo', pk=artigo.pk)
+    else:
+        form = ArtigoForm(instance=artigo)
+    return render(request, 'artigos/edicao_artigo.html', {'form': form})
