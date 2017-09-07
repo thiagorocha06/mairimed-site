471c1337a824300e84d6b585db097ecaaf3cf8b4
Thiago Rocha <thiagorocha06@gmail.com>
2017-09-07 10:52:59 -0300

    Adicionado static url para uso local e adicionado imagem logo no titulo. Alterado as cores dos topicos

--- mairimed/urls.py
+++ mairimed/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views
 
@@ -9,3 +11,7 @@ urlpatterns = [
     url(r'', include('artigos.urls')),
     url(r'', include('contas.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
