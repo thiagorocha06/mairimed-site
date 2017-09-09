362b54c91cdf3a66ff490a055e1e6763bf6e6e19
Thiago Rocha <thiagorocha06@gmail.com>
2017-09-09 13:47:47 -0300

    Adicionado referencias e inicio da barra inferior.

--- mairimed/settings/base.py
+++ mairimed/settings/base.py
@@ -27,7 +27,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'mairimed.pythonanywhere.com'
+    'mairimed.pythonanywhere.com',
+    'mairimed.com',
+    'www.mairimed.com',
 ]
 
 
--- mairimed/urls.py
+++ mairimed/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.contrib.auth import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^administracao/', admin.site.urls),
     #url(r'^accounts/login/$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('artigos.urls')),
