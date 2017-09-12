3d22451c8e9a6fe32b367fdd2094e8eeacd2e228
Thiago Rocha <thiagorocha06@gmail.com>
2017-09-12 10:04:59 -0300

    Adicionado novos campos ao exame fisico, etiologia e exames complementares; Correcao do ecg_respostas.

--- artigos/views.py
+++ artigos/views.py
@@ -37,19 +37,27 @@ def categorias_exercicios(request):
     return render(request, 'exercicios/categorias_exercicios.html')
 
 def exercicios_ecg(request):
-    exercicios = Exercicio.objects.all()
     exercicios = Exercicio.objects.filter(nome__isnull=False).order_by("pk")
     return render(request, 'exercicios/exercicios_ecg.html', {'exercicios' : exercicios})
 
 def exercicios_rx(request):
     return render(request, 'exercicios/exercicios_rx.html')
 
-def exercicios_resposta(request):
-    exercicios = Exercicio.objects.filter(nome__isnull=False).order_by('nome')
+#def exercicios_resposta(request):
+#    exercicios = Exercicio.objects.filter(nome__isnull=False).order_by('nome')
     #exercicio_resposta = get_object_or_404(Exercicio, pk=pk)
-    return render(request, 'exercicios/exercicios_resposta.html', {'exercicios' : exercicios})
+#    return render(request, 'exercicios/exercicios_resposta.html', {'exercicios' : exercicios})
     #return redirect(request.META['HTTP_REFERER'])
     #return redirect(instance.get_absolute_url())
+
+def exercicios_resposta(request):
+    exercicios = Exercicio.objects.filter(nome__isnull=False).order_by('pk')
+    mostrar_r = False
+    if request.GET.get("mostrar_resposta"):
+        mostrar_r = True
+    #exercicio_resposta = get_object_or_404(Exercicio, pk=pk)
+    return render(request, 'exercicios/exercicios_ecg.html', {'exercicios' : exercicios, 'mostrar_r' : mostrar_r})
+
 
 ### ARTIGOS ###
 
