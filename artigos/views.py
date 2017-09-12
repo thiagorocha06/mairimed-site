a9985e7e3116057dbc94acde6587059cc70f3454
Thiago Rocha <thiagorocha06@gmail.com>
2017-09-12 10:15:20 -0300

    Correcao do anteriorvoltando o view para dois commits atras...

--- artigos/views.py
+++ artigos/views.py
@@ -43,20 +43,20 @@ def exercicios_ecg(request):
 def exercicios_rx(request):
     return render(request, 'exercicios/exercicios_rx.html')
 
-#def exercicios_resposta(request):
-#    exercicios = Exercicio.objects.filter(nome__isnull=False).order_by('nome')
+def exercicios_resposta(request):
+    exercicios = Exercicio.objects.filter(nome__isnull=False).order_by('nome')
     #exercicio_resposta = get_object_or_404(Exercicio, pk=pk)
-#    return render(request, 'exercicios/exercicios_resposta.html', {'exercicios' : exercicios})
+    return render(request, 'exercicios/exercicios_resposta.html', {'exercicios' : exercicios})
     #return redirect(request.META['HTTP_REFERER'])
     #return redirect(instance.get_absolute_url())
 
-def exercicios_resposta(request):
-    exercicios = Exercicio.objects.filter(nome__isnull=False).order_by('pk')
-    mostrar_r = False
-    if request.GET.get("mostrar_resposta"):
-        mostrar_r = True
+#def exercicios_resposta(request):
+#    exercicios = Exercicio.objects.filter(nome__isnull=False).order_by('pk')
+#    mostrar_r = False
+#    if request.GET.get("mostrar_resposta"):
+#        mostrar_r = True
     #exercicio_resposta = get_object_or_404(Exercicio, pk=pk)
-    return render(request, 'exercicios/exercicios_ecg.html', {'exercicios' : exercicios, 'mostrar_r' : mostrar_r})
+#    return render(request, 'exercicios/exercicios_ecg.html', {'exercicios' : exercicios, 'mostrar_r' : mostrar_r})
 
 
 ### ARTIGOS ###
