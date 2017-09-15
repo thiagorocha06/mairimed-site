59189562400d75b3f4f02da1f45d641130a33d7a
Thiago Rocha <thiagorocha06@gmail.com>
2017-09-15 16:38:55 -0300

    Adicionado dinamica ao Admin.

--- artigos/admin.py
+++ artigos/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from artigos.models import Artigo, Termo, Farmaco
 
-admin.site.register(Artigo)
+class ArtigoModelAdmin(admin.ModelAdmin):
+    list_display = ["titulo", "data_de_criacao", "data_de_publicacao", "categoria", "modulo"]
+    list_filter = ["categoria", "modulo"]
+    search_fields = ["titulo"]
+    class meta:
+        model = Artigo
+
+admin.site.register(Artigo, ArtigoModelAdmin)
 admin.site.register(Farmaco)
 admin.site.register(Termo)
