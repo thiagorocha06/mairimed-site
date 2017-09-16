b32d8959d004ffd439c48f20eaefc8c5f45ec8b2
Thiago Rocha <thiagorocha06@gmail.com>
2017-09-16 10:25:21 -0300

    Adicionado barra alfabetica.

--- dicionario_medico/urls.py
+++ dicionario_medico/urls.py
@@ -3,7 +3,32 @@ from . import views
 
 urlpatterns = [
 
-### TERMOS ###
     url(r'^termos/(?P<pk>\d+)/$', views.detalhe_termos, name='detalhe_termos'),
     url(r'^termos/lista$', views.lista_termos, name='lista_termos'),
+
+## ALFABETO
+    url(r'^termos/termo_a$', views.termo_alfabetico, name='termo_a'),
+    url(r'^termos/termo_b$', views.termo_alfabetico, name='termo_b'),
+    url(r'^termos/termo_c$', views.termo_alfabetico, name='termo_c'),
+    url(r'^termos/termo_d$', views.termo_alfabetico, name='termo_d'),
+    url(r'^termos/termo_e$', views.termo_alfabetico, name='termo_e'),
+    url(r'^termos/termo_f$', views.termo_alfabetico, name='termo_f'),
+    url(r'^termos/termo_g$', views.termo_alfabetico, name='termo_g'),
+    url(r'^termos/termo_h$', views.termo_alfabetico, name='termo_h'),
+    url(r'^termos/termo_i$', views.termo_alfabetico, name='termo_i'),
+    url(r'^termos/termo_j$', views.termo_alfabetico, name='termo_j'),
+    url(r'^termos/termo_k$', views.termo_alfabetico, name='termo_k'),
+    url(r'^termos/termo_l$', views.termo_alfabetico, name='termo_l'),
+    url(r'^termos/termo_m$', views.termo_alfabetico, name='termo_m'),
+    url(r'^termos/termo_n$', views.termo_alfabetico, name='termo_n'),
+    url(r'^termos/termo_o$', views.termo_alfabetico, name='termo_o'),
+    url(r'^termos/termo_p$', views.termo_alfabetico, name='termo_p'),
+    url(r'^termos/termo_q$', views.termo_alfabetico, name='termo_q'),
+    url(r'^termos/termo_r$', views.termo_alfabetico, name='termo_r'),
+    url(r'^termos/termo_s$', views.termo_alfabetico, name='termo_s'),
+    url(r'^termos/termo_t$', views.termo_alfabetico, name='termo_t'),
+    url(r'^termos/termo_u$', views.termo_alfabetico, name='termo_u'),
+    url(r'^termos/termo_v$', views.termo_alfabetico, name='termo_v'),
+    url(r'^termos/termo_x$', views.termo_alfabetico, name='termo_x'),
+    url(r'^termos/termo_z$', views.termo_alfabetico, name='termo_z'),
 ]
--- dicionario_medico/views.py
+++ dicionario_medico/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Termo
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
-### TERMOS ###
+from django.core.urlresolvers import resolve
 
 def lista_termos(request):
     lista_termos = Termo.objects.filter(definicao__isnull=False).order_by('nome')
@@ -35,3 +34,85 @@ def lista_termos(request):
 def detalhe_termos(request, pk):
     termo = get_object_or_404(Termo, pk=pk)
     return render(request, 'termos/detalhe_termos.html', {'termo': termo})
+
+
+### ALFABETO ###
+
+def termo_alfabetico(request):
+    current_url = resolve(request.path_info).url_name
+    if current_url == 'termo_a':
+        letra = "A"
+    if current_url == 'termo_b':
+        letra = "B"
+    if current_url == 'termo_c':
+        letra = "C"
+    if current_url == 'termo_d':
+        letra = "D"
+    if current_url == 'termo_e':
+        letra = "E"
+    if current_url == 'termo_f':
+        letra = "F"
+    if current_url == 'termo_g':
+        letra = "G"
+    if current_url == 'termo_h':
+        letra = "H"
+    if current_url == 'termo_i':
+        letra = "I"
+    if current_url == 'termo_j':
+        letra = "J"
+    if current_url == 'termo_k':
+        letra = "K"
+    if current_url == 'termo_l':
+        letra = "L"
+    if current_url == 'termo_m':
+        letra = "M"
+    if current_url == 'termo_n':
+        letra = "N"
+    if current_url == 'termo_o':
+        letra = "O"
+    if current_url == 'termo_p':
+        letra = "P"
+    if current_url == 'termo_q':
+        letra = "Q"
+    if current_url == 'termo_r':
+        letra = "R"
+    if current_url == 'termo_s':
+        letra = "S"
+    if current_url == 'termo_t':
+        letra = "T"
+    if current_url == 'termo_u':
+        letra = "U"
+    if current_url == 'termo_v':
+        letra = "V"
+    if current_url == 'termo_x':
+        letra = "X"
+    if current_url == 'termo_z':
+        letra = "Z"
+    lista_termos = Termo.objects.filter(definicao__isnull=False).order_by('nome')
+    lista_termos_a = lista_termos.filter(nome__istartswith=letra).order_by('nome')
+
+    #Pesquisa
+    termo_pesquisa = request.GET.get("campo_pesquisa")
+    if termo_pesquisa:
+        lista_termos = lista_termos.filter(nome__icontains=termo_pesquisa)
+
+    #Paginacao
+    paginator = Paginator(lista_termos_a, 25) # Show 5 contacts per page
+    page_request_var = "pagina"
+    page = request.GET.get(page_request_var)
+    try:
+        termos = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        termos = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        termos = paginator.page(paginator.num_pages)
+
+    conteudo = {
+        'termos' : termos,
+        'page_request_var': page_request_var,
+        'letra' : letra
+    }
+
+    return render(request, 'termos/termo_alfabetico.html', conteudo)
