c745ec8ac1d3c3135606fd8a051509402a794c4e
Thiago Rocha <thiagorocha06@gmail.com>
2017-09-26 19:46:12 -0300

    Adicionado 3 serie.

--- artigos/urls.py
+++ artigos/urls.py
@@ -19,15 +19,37 @@ urlpatterns = [
     url(r'^artigo/(?P<pk>\d+)/favoritos/$', views.favoritos_artigo, name='favoritos_artigo'),
 
 ### ESCS ###
-    url(r'^escs/1_serie/$', views.categorias_artigos, name='1_serie'),
-    url(r'^escs/2_serie/$', views.categorias_artigos, name='2_serie'),
     url(r'^escs/3_serie/$', views.categorias_artigos, name='3_serie'),
+    url(r'^escs/ha_3/$', views.categorias_artigos, name='ha_3'),
     url(r'^escs/4_serie/$', views.categorias_artigos, name='4_serie'),
+    url(r'^escs/ha_4/$', views.categorias_artigos, name='ha_4'),
 
+# 3ª serie - MT
+    url(r'^escs/M301/$', views.escs_artigos, name='M301'),
+    url(r'^escs/M302/$', views.escs_artigos, name='M302'),
+    url(r'^escs/M303/$', views.escs_artigos, name='M303'),
+    url(r'^escs/M304/$', views.escs_artigos, name='M304'),
+    url(r'^escs/M306/$', views.escs_artigos, name='M306'),
+    url(r'^escs/M307/$', views.escs_artigos, name='M307'),
+# 3ª serie - HA
+    url(r'^escs/ha_ped/$', views.escs_artigos, name='ha_ped'),
+    url(r'^escs/ha_abdome/$', views.escs_artigos, name='ha_abdome'),
+    url(r'^escs/ha_cm/$', views.escs_artigos, name='ha_cm'),
+    url(r'^escs/ha_neurolocom/$', views.escs_artigos, name='ha_neurolocom'),
+
+# 4ª serie - MT
     url(r'^escs/M401/$', views.escs_artigos, name='M401'),
     url(r'^escs/M402/$', views.escs_artigos, name='M402'),
     url(r'^escs/M403/$', views.escs_artigos, name='M403'),
     url(r'^escs/M404/$', views.escs_artigos, name='M404'),
     url(r'^escs/M406/$', views.escs_artigos, name='M406'),
     url(r'^escs/M407/$', views.escs_artigos, name='M407'),
+
+# 4ª serie - HA
+    url(r'^escs/ha_ped_emerg/$', views.escs_artigos, name='ha_ped_emerg'),
+    url(r'^escs/ha_ped_amb/$', views.escs_artigos, name='ha_ped_amb'),
+    url(r'^escs/ha_cm_emerg/$', views.escs_artigos, name='ha_cm_emerg'),
+    url(r'^escs/ha_cm_amb/$', views.escs_artigos, name='ha_cm_amb'),
+    url(r'^escs/ha_cirurgia/$', views.escs_artigos, name='ha_cirurgia'),
+    url(r'^escs/ha_go/$', views.escs_artigos, name='ha_go'),
 ]
